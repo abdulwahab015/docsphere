d70eca8c532a799f316e2962712f4ef9aa6fe771
Abdul Wahab <abdulwahabihsan5@gmail.com>
2026-09-14 12:42:31 +0500

    feat: add document CRUD API with org-scoped access and restore

--- projects/models.py
+++ projects/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 from core.models import TimeStampedModel
 from projects.choices import AccessLevel
-from projects.managers import ProjectManager
+from projects.managers import DocumentManager, ProjectManager
 
 
 class Project(TimeStampedModel):
@@ -50,6 +50,8 @@ class Document(TimeStampedModel):
 
     title = models.CharField(max_length=100, db_index=True)
     content = models.TextField(null=True, blank=True)
+
+    objects = DocumentManager()
 
     def __str__(self):
         return self.title
