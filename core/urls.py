8908f7006b7b20bcbbc74f6e6b33cd2252ca1c2f
Abdul Wahab <abdul.wahab@cogentlabs.co>
2026-09-17 17:50:36 +0500

    feat: add organization self-signup and profile management endpoints (#18)

--- core/urls.py
+++ core/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
         name="swagger_ui",
     ),
     path("api/v1/users/", include("users.api.v1.urls")),
+    path("api/v1/organizations/", include("organizations.api.v1.urls")),
     path("api/v1/projects/", include("projects.api.v1.urls")),
     path("api/v1/documents/", include(document_urlpatterns)),
     path("api/v1/subscriptions/", include("subscriptions.api.v1.urls")),
